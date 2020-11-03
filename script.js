d2303a94330aa888f841ad9a898cd768a738c0db
dibran_ho <dibran_ho@outlook.com>
2020-11-03 11:45:52 +0100

    Created a function to display a card for every product in data

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-import products from './productsData/products.js'
+
 import data from './productsData/products.js';
 
 console.log(data);
+displayCards();
 
+function displayCards() {
+
+    var cardsHtml = ""
+    data.forEach(() => {
+        cardsHtml += `
+        <div class="slider-box">
+            <div class="slider-img">
+                <img src="https://static.toiimg.com/photo/71452088/Apple-iPhone-12-Pro.jpg" alt="">
+            </div>
+            <div class="slider-details">
+                <div class="type">
+                    <a href="#">Apple iPhone 12 Pro</a>
+                </div>
+                <div class="price-container">
+                    <a href="#"><span class="price">1100 &euro;</span></a>
+                </div>
+            </div>
+        </div>
+        `
+    });
+
+    document.querySelector('div.slider-container').innerHTML = cardsHtml;
+}
