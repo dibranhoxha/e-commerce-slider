cc7db9ad2222790afbc721301ae4a84e96230452
dibran_ho <dibran_ho@outlook.com>
2020-11-06 13:30:28 +0100

    Fixed error of loading data

--- script.js
+++ script.js
@@ -1,17 +1,192 @@
-import iPhoneData from './products-data/products.js';
-
 let sliderContainer = document.querySelector('div.slider-container');
 let leftArrow = document.querySelector('div.navigation-left');
 let rightArrow = document.querySelector('div.navigation-right');
 
+const iphoneData = [
+    {
+        id: 1,
+        name: 'Apple iphone 12 Pro max',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/150304-migration-iPhone12Pro_Max-retina.png',
+        price: 1500,
+        feedback: '&starf;'
+    },
+    {
+        id: 2,
+        name: 'Apple iphone 12 Pro',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/150304-migration-iPhone12Pro-retina.png',
+        price: 1300,
+        feedback: '&starf;'
+    },
+    {
+        id: 3,
+        name: 'Apple iPhone 12',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/150304-migration-iPhone12-retina.png',
+        price: 1100,
+        feedback: '&starf;'
+    },
+    {
+        id: 4,
+        name: 'Apple iPhone 11 Pro max',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/149432-compare-page-iphone11ProMax.png',
+        price: 999,
+        feedback: '&starf;'
+    },
+    {
+        id: 5,
+        name: 'Apple iPhone 11 Pro',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/149432-compare-page-iphone11PRO.png',
+        price: 900,
+        feedback: '&starf;'
+    },
+    {
+        id: 6,
+        name: 'Apple iPhone 11',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/149432-compare-page-iphone11.png',
+        price: 799,
+        feedback: '&starf;'
+    },
+    {
+        id: 7,
+        name: 'Apple iPhone XS max',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/149432-compare-page-iphoneXsMax.png',
+        price: 650,
+        feedback: '&starf;'
+    },
+    {
+        id: 8,
+        name: 'Apple iPhone XS',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/149432-compare-page-iphoneXs.png',
+        price: 599,
+        feedback: '&starf;'
+    },
+    {
+        id: 9,
+        name: 'Apple iPhone XR',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/149432-compare-page-iphoneXR.png',
+        price: 550,
+        feedback: '&starf;'
+    },
+    {
+        id: 10,
+        name: 'Apple iPhone X',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/149432-compare-page-iphoneXs.png',
+        price: 480,
+        feedback: '&starf;'
+    },
+    {
+        id: 11,
+        name: 'Apple iPhone 8 Plus',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/149432-compare-page-iphone8Plus.png',
+        price: 330,
+        feedback: '&starf;'
+    },
+    {
+        id: 1,
+        name: 'Apple iphone 12 Pro max',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/150304-migration-iPhone12Pro_Max-retina.png',
+        price: 1500,
+        feedback: '&starf;'
+    },
+    {
+        id: 2,
+        name: 'Apple iphone 12 Pro',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/150304-migration-iPhone12Pro-retina.png',
+        price: 1300,
+        feedback: '&starf;'
+    },
+    {
+        id: 3,
+        name: 'Apple iPhone 12',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/150304-migration-iPhone12-retina.png',
+        price: 1100,
+        feedback: '&starf;'
+    },
+    {
+        id: 4,
+        name: 'Apple iPhone 11 Pro max',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/149432-compare-page-iphone11ProMax.png',
+        price: 999,
+        feedback: '&starf;'
+    },
+    {
+        id: 5,
+        name: 'Apple iPhone 11 Pro',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/149432-compare-page-iphone11PRO.png',
+        price: 900,
+        feedback: '&starf;'
+    },
+    {
+        id: 6,
+        name: 'Apple iPhone 11',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/149432-compare-page-iphone11.png',
+        price: 799,
+        feedback: '&starf;'
+    },
+    {
+        id: 7,
+        name: 'Apple iPhone XS max',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/149432-compare-page-iphoneXsMax.png',
+        price: 650,
+        feedback: '&starf;'
+    },
+    {
+        id: 8,
+        name: 'Apple iPhone XS',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/149432-compare-page-iphoneXs.png',
+        price: 599,
+        feedback: '&starf;'
+    },
+    {
+        id: 9,
+        name: 'Apple iPhone XR',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/149432-compare-page-iphoneXR.png',
+        price: 550,
+        feedback: '&starf;'
+    },
+    {
+        id: 10,
+        name: 'Apple iPhone X',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/149432-compare-page-iphoneXs.png',
+        price: 480,
+        feedback: '&starf;'
+    },
+    {
+        id: 11,
+        name: 'Apple iPhone 8 Plus',
+        description: 'asdfasdfasdf',
+        photoUrl: 'https://www.att.com/idpassets/sales/uf/compare/iphone-compare-page/149432-compare-page-iphone8Plus.png',
+        price: 330,
+        feedback: '&starf;'
+    },
+];
+
 checkScroll();
-displayCards();
+displayCards(iphoneData);
 leftArrow.addEventListener('click', () => scroll(leftArrow.className));
 rightArrow.addEventListener('click', () => scroll(rightArrow.className));
 
-
-
-function displayCards() {
+function displayCards(iPhoneData) {
 
     var cardsHtml = ""
     iPhoneData.map(product => {
